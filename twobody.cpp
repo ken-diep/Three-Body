3a5886e21b1924d0c74df4dc81cafacd5bedb245
ken-diep <originalkd@gmail.com>
2026-03-01 14:34:17 +0000

    added trail to sfml

--- twobody.cpp
+++ twobody.cpp
@@ -1,12 +1,6 @@
-#include <iostream>
-#include <vector>
 #include <string>
 #include <cmath>
-#include <thread>   
-#include <chrono>   
-#include <utility>
 #include <SFML/Graphics.hpp>
-
 
 using namespace std;
 
@@ -16,7 +10,15 @@ const double AU = 1.496e11; // 1 Astronomical Unit (meters)
 const double MASS_SUN = 1.989e30;
 const double MASS_EARTH = 5.972e24;
 
+enum class OrbitMethod {
+    Euler,
+    SemiImplicitEuler,
+    VelocityVerlet,
+    RK4 // (For future expansion)
+};
 
+// Set this variable to swap methods!
+OrbitMethod currentMethod = OrbitMethod::Euler;
 
 struct Vec3 {
     double x, y, z;
@@ -58,12 +60,21 @@ void update_body(Body& body, const Vec3& acceleration, double dt) {
     body.position = body.position + body.velocity * dt;
 }
 
+void updatePhysics(Body& earth, const Body& sun, double dt, OrbitMethod method) {
+    
+}
 
 int main() {
     //Initialise SFML window
     sf::RenderWindow window(sf::VideoMode({1000, 1000}), "Two-Body Orbit - SFML 3");
     window.setFramerateLimit(60);
     
+    sf::Font font;
+    if (!font.openFromFile("C:/Windows/Fonts/arial.ttf")) return -1;
+    sf::Text hud(font, "", 20);
+    hud.setFillColor(sf::Color::White);
+    hud.setPosition({10.f, 10.f});
+
     double dt = 40000.0; 
     Body sun(MASS_SUN, Vec3(0,0,0), Vec3(0,0,0));
     Body earth(MASS_EARTH, Vec3(1.496e11, 0, 0), Vec3(0, 2.978e4, 0));
@@ -82,6 +93,13 @@ int main() {
     earthShape.setFillColor(sf::Color(100, 180, 255));
     earthShape.setOrigin({8.f, 8.f});
 
+    // Create a vertex array for the trail
+    sf::VertexArray trail(sf::PrimitiveType::LineStrip);
+
+    // Optional: Limit trail length so it doesn't grow forever
+    const size_t maxTrailPoints = 1000;
+
+    double daysPassed = 0; 
 
     while (window.isOpen()) {
 
@@ -95,8 +113,16 @@ int main() {
         Vec3 force_earth = calculate_grav_force(earth, sun);
         update_body(earth, force_earth / earth.mass, dt);
 
+        daysPassed += dt / 86400.0; // Convert seconds to days
+
+        // --- Update Text ---
+        std::string status = "Two-body simulation of Earth orbiting around Sun, using semi-implicit Euler" 
+                            "\nDays: " + std::to_string((int)daysPassed) + 
+                             "\nVel: " + std::to_string((int)earth.velocity.magnitude()) + " m/s";
+        hud.setString(status);
+
         // --- Rendering (Projecting 3D to 2D) ---
-        window.clear(sf::Color(10, 10, 25));
+        window.clear(sf::Color(5, 5, 15));
 
         window.draw(sunShape);
 
@@ -105,7 +131,18 @@ int main() {
         float ey = center.y + static_cast<float>(earth.position.y) * screenScale;
         earthShape.setPosition({ex, ey});
 
+        sf::Vector2f currentPos = { ex, ey };
+        sf::Color trailColor = sf::Color(0, 255, 255, 100); // Cyan with transparency
+
+        // Explicitly creating the Vertex
+        sf::Vertex v{currentPos, trailColor};
+        trail.append(v);
+
         window.draw(earthShape);
+
+        window.draw(trail);
+        
+        window.draw(hud); // Draw the text last so it's on top
         window.display();
     }
 
